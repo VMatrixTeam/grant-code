e3342e6218ee4ce4bbfa5ee6857b767dc21e9ab7
Skida12138 <su1q2d2@163.com>
2019-12-28 02:23:52 +0800

    （试图）将时间与github同步

--- js/grant-code.js
+++ js/grant-code.js
@@ -1,5 +1,5 @@
 window.refreshGrantCode = function() {
   var secret = window.location.hash.slice(2);
-  var token = window.otplib.totp.generate(secret);
+  var token = window.otplib.totp.generate(secret, { epoch: Date.now()+window.timeOffset });
   document.getElementById('grant-code').innerText = token;
 }
--- js/set-timer.js
+++ js/set-timer.js
@@ -7,14 +7,21 @@ window.updateExpireTime = function(count) {
 }
 
 window.setTimer = function() {
-  var count = Math.floor((new Date().getTime() % 30000)/1000+0.5);
-  window.updateExpireTime(count);
-  window.setInterval(function() {
-    count++;
-    if (count === window.constants.MAX_EXPIRE_TIME()) {
-      count = 0;
-      window.refreshGrantCode();
-    }
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', window.location, true);
+  xhr.onload = function() {
+    var epoch = new Date(xhr.getResponseHeader('date')).getTime();
+    window.timeOffset = epoch - Date.now();
+    var count = Math.floor((epoch % 30000)/1000+0.5);
     window.updateExpireTime(count);
-  }, 1000);
+    window.setInterval(function() {
+      count++;
+      if (count === window.constants.MAX_EXPIRE_TIME()) {
+        count = 0;
+        window.refreshGrantCode();
+      }
+      window.updateExpireTime(count);
+    }, 1000);
+  }
+  xhr.send(null);
 };

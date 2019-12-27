f2e35a0bd68e43b433b32339031773a321ce136b
SYSU101 <su1q2d2@163.com>
2019-12-28 02:56:45 +0800

    使用progress事件而不是load事件获取时间戳以取得更精确的同步时间

--- js/set-timer.js
+++ js/set-timer.js
@@ -9,19 +9,21 @@ window.updateExpireTime = function(count) {
 window.setTimer = function() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', window.location, true);
-  xhr.onload = function() {
-    var epoch = new Date(xhr.getResponseHeader('date')).getTime();
-    window.timeOffset = epoch - Date.now();
-    var count = Math.floor((epoch % 30000)/1000+0.5);
-    window.updateExpireTime(count);
-    window.setInterval(function() {
-      count++;
-      if (count === window.constants.MAX_EXPIRE_TIME()) {
-        count = 0;
-        window.refreshGrantCode();
-      }
+  xhr.onprogress = function() {
+    if (window.timeOffset === void 0) {
+      var epoch = new Date(xhr.getResponseHeader('date')).getTime();
+      window.timeOffset = epoch - Date.now();
+      var count = Math.floor((epoch % 30000)/1000+0.5);
       window.updateExpireTime(count);
-    }, 1000);
+      window.setInterval(function() {
+        count++;
+        if (count === window.constants.MAX_EXPIRE_TIME()) {
+          count = 0;
+          window.refreshGrantCode();
+        }
+        window.updateExpireTime(count);
+      }, 1000);
+    }
   }
   xhr.send(null);
 };

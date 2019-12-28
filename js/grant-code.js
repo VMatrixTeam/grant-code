e659e8b8a00d81fe81fee433a4f3c9aa539be104
SYSU101 <su1q2d2@163.com>
2019-12-28 19:45:48 +0800

    更换otpp生成算法

--- js/grant-code.js
+++ js/grant-code.js
@@ -1,6 +1,7 @@
 window.refreshGrantCode = function() {
   var secret = window.location.hash.slice(2).toUpperCase();
+  console.log(secret);
   window.otplib.totp.options = { epoch: Date.now()+window.timeOffset };
-  var token = window.otplib.totp.generate(secret);
+  var token = window.otplib.authenticator.generate(secret);
   document.getElementById('grant-code').innerText = token;
 }
